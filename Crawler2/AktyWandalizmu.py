1b5046a9f1c7a2ff0c22e28892479526a6873502
Reaper <reaper_93@o2.pl>
2015-08-30 23:48:21 +0200

    Added detection of whole paragraph deletion, better output formatting

--- Crawler2/AktyWandalizmu.py
+++ Crawler2/AktyWandalizmu.py
@@ -18,6 +18,7 @@ class Zmiany(object):
         self.iloscZnakow = 0          #liczba zmienionych znaków
         self.iloscZmian = 0           #ilosc nastepujących po sobie + i - (lub odwrotnie)
         self.usuwany = False          #czy akapit został usunięty i jest przywracany czy odwrotnie
+        self.usunietoCaly = False     #czy złośliwie usunięto cały wpis
         self.komentarz = ""           #komentarz do wykonanej zmiany (dodadny przez autora wpisu)
         self.urlCur = ""              #link do cur
         self.urlPrev = ""             #link do prev
@@ -29,8 +30,10 @@ class AktyWandalizmu(object):
         self.strona = "https://en.wikipedia.org/w/index.php?title=God&offset=&limit=500&action=history"
         self.rewizje = []       #kontener na linki do rewizji powyżej 200 zmian
         self.liczbaZmian = 200  #liczba zmian do wyszukania
+        self.calyAkapit = 20000 #liczba powyżej której uznajemy że cały akapit został usunięty
         self.wojny = {}         #słownik zawierający pary id: powiązane zmiany
         self.rezultat = ""      #wyniki do zwrócenia
+
     def wyszukajZmiany(self):
         "Pobiera linki do wszystkich rewizji które mają więcej niż 200 zmian"
         historia = przetworzStrone(self.strona).find(id="pagehistory").find_all("li")    #w stronie historii wyciągnij wszystkie wpisy zmian
@@ -39,7 +42,8 @@ class AktyWandalizmu(object):
         for odnosnik in historia:
             for span in odnosnik.find_all(re.compile('span|strong'), class_=re.compile("mw-plusminus-")):   #wyszukaj wszystkie wystapienia klas mw-plusminus-pos oraz mw-plusminus-neg czyli ilość zmian
                 liczba = int(span.string[1:-1].replace(',', ''))         #zamiana przecinka na nic czyli z 23,444 zrobi 23444 (inaczej będą błędy)       
-                if abs(liczba) > self.liczbaZmian:      #jeżeli liczba zmian jest większa niż zakładana              
+                if abs(liczba) > self.liczbaZmian:      #jeżeli liczba zmian jest większa niż zakładana      
+                    self.rezultat += "\n"               #żeby to jakoś wyglądało w przeglądarce        
                     zmiana = Zmiany()
                     zmiana.iloscZnakow = abs(liczba)
                     zmiana.odnosnik = odnosnik
@@ -47,45 +51,44 @@ class AktyWandalizmu(object):
                         zmiana.usuwany = False
                     else:
                         zmiana.usuwany = True
+                        if abs(liczba) > self.calyAkapit:   #prawdopodobnie usunięto cały akapit
+                            zmiana.usunietoCaly = True
+                            print("Usunieto caly akapit");
+                            self.rezultat += "Usunieto caly akapit \n "
                     komentarz = odnosnik.find('span', class_="comment")     #poszukaj komentarza
                     try:
-                        print("Komentarz: " + unicode(komentarz.text).encode('ascii', 'ignore')) + "<br>"
-                        self.rezultat += str("Komentarz: " + unicode(komentarz.text).encode('ascii', 'ignore')) + " </br> "
+                        print("Komentarz: " + unicode(komentarz.text).encode('ascii', 'ignore'))
+                        self.rezultat += str("Komentarz: " + unicode(komentarz.text).encode('ascii', 'ignore')) + " \n "
                         zmiana.komentarz = unicode(komentarz.text).encode('ascii', 'ignore')
                     except AttributeError:
                         pass        #nie było opisu przy zmianie (zdarza się to jednak dość rzadko
-                    cur = odnosnik.find('span', class_="mw-history-histlinks").find_all("a")
-                    #cur = cur.find_all('a')
-                    first = True
+                    cur = odnosnik.find('span', class_="mw-history-histlinks").find_all("a")    #wyszukanie linków do prev i cur
+                    first = True    #pierwszy to zawsze cur a drugi prev
                     for link in cur:
                         if first:
                             print("Cur: " + "https://en.wikipedia.org/" + str(link.get('href')))
-                            self.rezultat += str("Cur: " + "https://en.wikipedia.org/" + str(link.get('href'))) + " </br> "
+                            self.rezultat += str("Cur: " + "https://en.wikipedia.org/" + str(link.get('href'))) + " \n "
                             first = False
                         else:
                             print("Prev: " + "https://en.wikipedia.org/" + str(link.get('href')))
-                            self.rezultat += str("Prev: " + "https://en.wikipedia.org/" + str(link.get('href'))) + " </br> "
+                            self.rezultat += str("Prev: " + "https://en.wikipedia.org/" + str(link.get('href'))) + " \n "
                     if id in self.wojny and next > 0:       #jeżeli zmiany są obok siebie oraz jest już wcześniej dodane to dopisuje do tego id w słowniku
                         self.wojny[id].append(zmiana)
                         print("Dodano" + str(span.string) + "z id: " + str(id))
-                        self.rezultat += str("Dodano" + str(span.string) + "z id: " + str(id)) + " </br> "
+                        self.rezultat += str("Dodano" + str(span.string) + "z id: " + str(id)) + " \n "
                         next += 1                           #kontynuujemy numerowanie - jeżeli zmiany są następujące po sobie to ta zmienna rośnie
                     elif id not in self.wojny or next == 0:
                         id += 1                 #zwiększ id o 1, za pierwszym razem ustawia na 0 (w sumie to nie ma znaczenia - to nie C)
                         self.wojny[id] = []     #dodaje do słownika o kluczu id nową liste na której będą obiekty reprezentujące zmiany tych samych paragrafów
                         self.wojny[id].append(zmiana)
                         print("Dodano" + str(span.string) + "z id: " + str(id))
-                        self.rezultat += str("Dodano" + str(span.string) + "z id: " + str(id)) + " </br> "
+                        self.rezultat += str("Dodano" + str(span.string) + "z id: " + str(id)) + " \n "
                         next += 1               #dodajemy aby było powyżej zera
                     else:
                         pass        #coś nie pykło (powinien być tutaj jakiś exception)
                 else:
                     if liczba != 0:        #zauważyłem że czasem pomiędzy dwoma zmianami jest jedna z numerem zmian równym 0, nie wiem dlaczego tak jest
                         next = 0
-                    #id += 1
-        #for item in self.wojny:
-        #    print item
-        #return self.wojny
 
     def odfiltrujPojedyncze(self):
         "Jeżeli nastąpiło dodanie tesktu lub jego usunięcie ale jest to jedynie uzupełnienie jakiejś informacji a nie ciągła zmiana akapitu to usuń takie wyniki"
@@ -93,9 +96,8 @@ class AktyWandalizmu(object):
         for key in self.wojny:
             if len(self.wojny[key]) > 1:        #jeżeli wystąpiło + - (lub - +), czyli więcej niż pojedynczy niepowiązany wpis
                 tylkoWojny[key] = self.wojny[key]
-                print tylkoWojny[key]
+                #print tylkoWojny[key]
         self.wojny = tylkoWojny
-        #return self.wojny
 
     def crawl(self):
         "Glówna funkcja, zwraca rezultat do przeglądarki"
